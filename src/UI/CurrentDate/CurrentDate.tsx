a0c1ed1ccfed212c12c94f06b5f3e768ccbf1d69
SergZu <sergzu0@gmail.com>
2021-11-02 23:59:22 +0600

    Some fixes for CurrentDate

--- src/UI/CurrentDate/CurrentDate.tsx
+++ src/UI/CurrentDate/CurrentDate.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import classes from './CurrentDate.module.css'
+
+export const days = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+];
+
+export const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'Jule', 'August', 'September', 'October', 'November', 'December'
+];
 
 const CurrentDate = () => {
     const now = new Date();
-    const days = [
-        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-    ];
-    const months = [
-        'January', 'February', 'March', 'April', 'May', 'June', 'Jule', 'August', 'September', 'October', 'November', 'December'
-    ];
+    
     return (
-        <span>
+        <span className={classes.currentDate}>
             {`${now.getHours()}:${now.getMinutes()} - ${days[ now.getDay() ]} - ${now.getDate()} ${months[ now.getMonth() ]} ${now.getFullYear() % 100}`}
         </span>
     )
--- src/utils/computeDirection.ts
+++ src/utils/computeDirection.ts
@@ -1,12 +1,12 @@
 
 const computeDirection = function(deg : number) : string {
     if (deg > 337.5 || deg < 22.5) return 'N'
-    if (deg < 67.5) return 'NE'
+    if (deg < 67.5) return 'N-E'
     if (deg < 112.5) return 'E'
-    if (deg < 157.5) return 'SE'
+    if (deg < 157.5) return 'S-E'
     if (deg < 202.5) return 'S'
-    if (deg < 247.5) return 'SW'
+    if (deg < 247.5) return 'S-W'
     if (deg < 292.5) return 'W'
-    return 'NW'
+    return 'N-W'
 }
 export default computeDirection;
