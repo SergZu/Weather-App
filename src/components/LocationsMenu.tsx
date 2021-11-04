0131bf95004d5088a79b4376a5f39c1c1026fc98
SergZu <sergzu0@gmail.com>
2021-11-04 23:40:33 +0600

    Add modal functionality

--- src/components/LocationsMenu.tsx
+++ src/components/LocationsMenu.tsx
@@ -1,6 +1,8 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import classes from './LocationsMenu.module.css'
 import { Location } from './App'
+import SearchModal from './SearchModal';
+import SimpleBtn from '../UI/SimpleBtn/SimpleBtn';
 
 export interface LocationMenuProps {
     list : Location[],
@@ -9,17 +11,30 @@ export interface LocationMenuProps {
 }
 
 const LocationsMenu = ({list, addLocation, deleteLocation} : LocationMenuProps) => {
+    const [isModalOpen, toggleModal] = useState(false);
+
+    const openModal = () => {
+        if (isModalOpen) return ;
+        toggleModal(true);
+        console.log('opened')
+    }
+
+    const closeModal = () => {
+        if (!isModalOpen) return ;
+        toggleModal(false);
+        console.log('closed')
+    }
+
     const createLocationsLayout = useMemo(() => (list : Location[]) =>
         list?.map((elem) => (<li key={`${elem.name + elem.lon + elem.lat}`} className={classes.locationElement}>{elem.name}</li>)), [list]) 
    
     return (
-        <div className={classes.locationsBoard}>
+        <div className={classes.locationsBoard}>            
+            {isModalOpen && (<SearchModal closeModal={closeModal} addLocation={addLocation} />)}
             <ul className={classes.locationList}>
             {createLocationsLayout(list)}
             </ul>
-            <button className={classes.locationAddBtn}>
-                +    
-            </button> 
+            <SimpleBtn onclickHandler={openModal} >&#43;</SimpleBtn>
         </div>
     )
 }
