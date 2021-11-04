5f91c2dbf21d3771d741f5eb43402fb4884e9d72
SergZu <sergzu0@gmail.com>
2021-11-04 22:27:07 +0600

    Add className prop for SimpleBtn

--- src/UI/SimpleBtn/SimpleBtn.tsx
+++ src/UI/SimpleBtn/SimpleBtn.tsx
@@ -4,11 +4,12 @@ import classes from './SimpleBtn.module.css'
 export interface SimpleBtnProps {
     onclickHandler : () => void;
     children : React.ElementType | string;
+    className? : string;
 }
 
-const SimpleBtn = ({children, onclickHandler} : SimpleBtnProps) => {
+const SimpleBtn = ({children, onclickHandler, className} : SimpleBtnProps) => {
     return (
-        <button className={classes.btn} onClick={onclickHandler}>
+        <button className={`${classes.btn} ${className}`} onClick={onclickHandler}>
            {children}
         </button>
     )
