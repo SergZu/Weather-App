d2feb4aa42ccc52c152f43c1c370c8023ceeed4c
SergZu <sergzu0@gmail.com>
2021-11-04 23:40:01 +0600

    Add some layout for SearchModal

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import classes from './SearchModal.module.css'
 import { Location } from './App'
+import SimpleBtn from '../UI/SimpleBtn/SimpleBtn'
+import SearchForm from './SearchForm'
 
 export interface SearchModalProps{
     closeModal : () => void;
@@ -10,7 +12,11 @@ export interface SearchModalProps{
 const SearchModal = ({closeModal, addLocation}) => {
     return (
         <div className={classes.modal}>
-            
+            <div>
+                <button>Get My Position</button>
+                <SearchForm addLocation={addLocation} />
+            </div>
+            <SimpleBtn className={classes.closeBtn} onclickHandler={closeModal}>&#10008;</SimpleBtn>
         </div>
     )
 }
