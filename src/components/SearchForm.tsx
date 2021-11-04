1e8fcaf2bbd103138d0fdf3df110e5fe72af95c6
SergZu <sergzu0@gmail.com>
2021-11-04 23:39:36 +0600

    Add basic functionality for Search Form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { Location } from './App';
 
-const SearchForm = () => {
+export interface SearchFormProps{
+    addLocation : (location : Location) => void;
+}
+
+const SearchForm = ({addLocation} : SearchFormProps) => {
+    const searchInput = useRef(null);
+    const searchLocation = (evt) => {
+        evt.preventDefault();
+        const value = searchInput.current.value;
+        addLocation({
+            name : value,
+            lat : '0',
+            lon : '0'
+        });
+    }
     return (
         <div>
-            
+           <form>
+                <input ref={searchInput} />
+                <button onClick={searchLocation}>Add</button>   
+            </form>
+            <div>
+                Results
+            </div> 
         </div>
     )
 }
