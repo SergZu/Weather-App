7a77f1481eaa80593b85961b0786b4c499f8a6f1
SergZu <sergzu0@gmail.com>
2021-11-25 19:45:58 +0600

    Add get geo position functionality

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './SearchModal.module.css'
 import { Location } from './App'
 import SimpleBtn from '../UI/SimpleBtn/SimpleBtn'
@@ -6,14 +6,44 @@ import SearchForm from './SearchForm'
 
 export interface SearchModalProps{
     closeModal : () => void;
-    addLocation : (location : Location) => void
+    addLocation : (location : Location) => void;
 }
 
+
 const SearchModal = ({closeModal, addLocation}) => {
+    const [ geoError, setGeoError ] = useState(null);
+    const [ geoPositionInProgress, setGeoPositionInProgress] = useState(false);
+
+    const onGeoClickHandler = () => {
+
+        if (geoPositionInProgress) return
+
+        setGeoPositionInProgress(true);
+
+        if (navigator.geolocation) {
+
+            const onSuccess = ({coords}) => {
+                addLocation({
+                    name : 'My last location',
+                    lat : coords.latitude,
+                    lon : coords.longitude
+                })
+                setGeoError(null);
+                setGeoPositionInProgress(false);
+            }
+
+            const onError = (error) => {
+                setGeoError(error.message);
+                setGeoPositionInProgress(false);
+            }
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        }
+    }
+
     return (
         <div className={classes.modal}>
             <div>
-                <button>Get My Position</button>
+                <button onClick={onGeoClickHandler}>Get My Position</button>
                 <SearchForm addLocation={addLocation} />
             </div>
             <SimpleBtn className={classes.closeBtn} onclickHandler={closeModal}>&#10008;</SimpleBtn>
