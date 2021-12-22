4acf9ac86bcd6891db63669c216222c367206eac
SergZu <sergzu0@gmail.com>
2021-12-22 22:46:50 +0600

    Alternate spinner ui component

--- src/UI/spinner/Spinner.tsx
+++ src/UI/spinner/Spinner.tsx
@@ -3,8 +3,10 @@ import classes from './Spinner.module.css';
 
 const Spinner = () => {
     return (
-        <div className={`${classes.loading} ${classes.spinner} `}>
-            <span className={classes.spinnerText}>Loading...</span>
+        <div className={classes.spinner}>
+            <div className={classes.loading}>
+                <span className={classes.text}>Loading...</span>
+            </div>
         </div>
     )
 }
