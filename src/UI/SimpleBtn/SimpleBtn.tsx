9f6555c68b3ccaf4710b203af2997534bc501080
SergZu <sergzu0@gmail.com>
2021-12-22 23:48:57 +0600

    Add hidden attr support

--- src/UI/SimpleBtn/SimpleBtn.tsx
+++ src/UI/SimpleBtn/SimpleBtn.tsx
@@ -5,11 +5,12 @@ export interface SimpleBtnProps {
     onclickHandler : () => void;
     children : React.ElementType | string;
     className? : string;
+    hidden : boolean;
 }
 
-const SimpleBtn = ({children, onclickHandler, className} : SimpleBtnProps) => {
+const SimpleBtn = ({children, onclickHandler, className, hidden} : SimpleBtnProps) => {
     return (
-        <button className={`${classes.btn} ${classes[className]}`} onClick={onclickHandler}>
+        <button className={`${classes.btn} ${classes[className]} ${hidden ? classes.invisible : null}`} onClick={onclickHandler}>
            {children}
         </button>
     )
